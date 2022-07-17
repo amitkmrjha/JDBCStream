82bd205d6885ec6d7e6128b0e603efec31ca8748
Amit Kumar <amitjha.manipal@gmail.com>
2022-07-17 23:19:21 +0200

    hello

--- ddl_scripts/V4__userGrants.sql
+++ ddl_scripts/V4__userGrants.sql
@@ -1,9 +1,9 @@
-GRANT SELECT, INSERT ON event_journal TO "migration-onboarding";
-GRANT SELECT, INSERT, UPDATE, DELETE ON event_tag TO "migration-onboarding";
-GRANT SELECT, INSERT, UPDATE, DELETE ON snapshot TO "migration-onboarding";
-GRANT SELECT, INSERT, UPDATE, DELETE ON durable_state TO "migration-onboarding";
+GRANT SELECT, INSERT ON event_journal TO supermo;
+GRANT SELECT, INSERT, UPDATE, DELETE ON event_tag TO supermo;
+GRANT SELECT, INSERT, UPDATE, DELETE ON snapshot TO supermo;
+GRANT SELECT, INSERT, UPDATE, DELETE ON durable_state TO supermo;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON akka_projection_offset_store TO "migration-onboarding";
-GRANT SELECT, INSERT, UPDATE, DELETE ON akka_projection_management TO "migration-onboarding";
+GRANT SELECT, INSERT, UPDATE, DELETE ON akka_projection_offset_store TO supermo;
+GRANT SELECT, INSERT, UPDATE, DELETE ON akka_projection_management TO supermo;
 
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO "migration-onboarding";
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO supermo;
